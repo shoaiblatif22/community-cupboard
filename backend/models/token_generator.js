f22dd81563967f6abab0b10dbf38b7d8817f6222
Shoaib Latif <shoaiblatif11@gmail.com>
2023-07-26 14:08:03 +0100

    updated file

--- backend/models/token_generator.js
+++ backend/models/token_generator.js
@@ -7,7 +7,6 @@ class TokenGenerator {
       user_id: user_id,
       iat: Math.floor(Date.now() / 1000),
       
-      // Set the JWT token to expire in 10 minutes
       exp: Math.floor(Date.now() / 1000) + (10 * 60)
     }, secret);
   }
