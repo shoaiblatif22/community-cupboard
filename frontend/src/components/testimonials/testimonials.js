19faa9f8797f363d329096a818c85b1f60b4ee15
Zara <zarabucknor@yahoo.co.uk>
2023-07-30 00:39:05 +0100

    testimonial route added

---
+++ frontend/src/components/testimonials/testimonials.js
@@ -0,0 +1,9 @@
+const Testimonials = () => {
+    return (
+        <p>
+            Happy that someone had an idea as genius as this. Loved using this app. The Volunteer even came in and helped me un pack my package. A* service!
+        </p>
+    )
+}
+
+export default Testimonials;
