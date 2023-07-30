d07459753b01e5a433d9ef5c7f7b6c01b1c0eae6
Zara <zarabucknor@yahoo.co.uk>
2023-07-30 01:21:24 +0100

    Initial design form added for users to send correspondance through email

--- frontend/src/components/contactUs/ContactUs.js
+++ frontend/src/components/contactUs/ContactUs.js
@@ -1,10 +1,34 @@
+import React from 'react';
+import './ContactUs.css';
+
 const ContactUs = () => {
-    return (
-        <><h1>Contact Us</h1><p>Please dont hesitate to Contact us:
-            Phone: 0208 446 1234
-            email: communitycupboard@info.org
-        </p></>
-    )
-}
+  return (
+    <div className="contact-container">
+      <h1>Contact Us</h1>
+      <div className="contact-info">
+        <div className="contact-item">
+          <i className="fa fa-map-marker"></i>
+          <p>1234 Elm Street<br />Your City, State<br />Postal Code</p>
+        </div>
+        <div className="contact-item">
+          <i className="fa fa-phone"></i>
+          <p>+1 (123) 456-7890</p>
+        </div>
+        <div className="contact-item">
+          <i className="fa fa-envelope"></i>
+          <p>info@communitycupboard.com</p>
+        </div>
+      </div>
+      <div className="contact-form">
+        <form>
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" />
+          <textarea placeholder="Your Message"></textarea>
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default ContactUs;
