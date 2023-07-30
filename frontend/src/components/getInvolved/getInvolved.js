14b9f5695e75bf0d6b5d22a32a1544e2efdb7873
Zara <zarabucknor@yahoo.co.uk>
2023-07-30 03:11:47 +0100

    sign up form added if you wanted to volunteer

--- frontend/src/components/getInvolved/getInvolved.js
+++ frontend/src/components/getInvolved/getInvolved.js
@@ -1,10 +1,33 @@
+import React from 'react';
+import './getInvolved.css';
+
 const GetInvolved = () => {
-    return (
-        <><h1>Get Involved </h1><p>If you share our passion for making a positive impact,
-            consider becoming a volunteer. Your time and effort can
-            bring smiles to the faces of those facing challenges,
-            making a meaningful difference in their lives.</p></>
-    )
-}
+  return (
+    <div className="get-involved-container">
+      <h1>Get Involved</h1>
+      <div className="get-involved-box">
+        <div className="get-involved-image">
+          {/* Placeholder image */}
+          <img src="path-to-image.jpg" alt="Volunteer" />
+        </div>
+        <div className="get-involved-text">
+          <p>
+            If you share our passion for making a positive impact, consider becoming a volunteer. Your time and effort can bring smiles to the faces of those facing challenges, making a meaningful difference in their lives.
+          </p>
+        </div>
+      </div>
+      <div className="get-involved-form">
+        <h2>Sign Up to Volunteer</h2>
+        <form>
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" />
+          <input type="tel" placeholder="Your Phone Number" />
+          <textarea placeholder="Tell us about your interests and availability"></textarea>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default GetInvolved;
