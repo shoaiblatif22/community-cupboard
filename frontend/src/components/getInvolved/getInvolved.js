43ef9187d36dd8249c1432ac3a953618ed246d40
Zara <zarabucknor@yahoo.co.uk>
2023-07-30 01:21:50 +0100

    text added to function

--- frontend/src/components/getInvolved/getInvolved.js
+++ frontend/src/components/getInvolved/getInvolved.js
@@ -1,6 +1,9 @@
 const GetInvolved = () => {
     return (
-        <h1>Volunteer here!</h1>
+        <><h1>Get Involved </h1><p>If you share our passion for making a positive impact,
+            consider becoming a volunteer. Your time and effort can
+            bring smiles to the faces of those facing challenges,
+            making a meaningful difference in their lives.</p></>
     )
 }
 
