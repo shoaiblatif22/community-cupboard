5e11a9b9fc2d280882e037aa37753752176b1f26
Della Izadi <delaram.ae@gmail.com>
2023-07-31 17:38:23 +0100

    Amended the app file to handle handlePackagesButtonClick

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,17 +1,14 @@
-
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import AppRoutes from "../Routes";
-import './App.css'; // Import the corresponding CSS file
+import "./App.css";
 import { motion } from "framer-motion";
 import CClogo from "./cc-logo.jpg.png";
 import SignUpForm from "../SignUp/SignUp.js";
 import LogInForm from "../login/LoginForm";
-import Notifications from '../notifications/notifications';
+import Notifications from "../notifications/notifications";
 import Carousel from "../banner/carousel";
-
-
 
 const App = () => {
   const [isSignUpOpen, setIsSignUpOpen] = useState(false);
@@ -26,7 +23,6 @@ const App = () => {
   };
 
   const [isLogInOpen, setIsLogInOpen] = useState(false);
- 
 
   const handleLogInButtonClick = () => {
     setIsLogInOpen(true);
@@ -36,59 +32,44 @@ const App = () => {
     setIsLogInOpen(false);
   };
 
-  // const handleHomeButtonClick = () => {
-  //   navigate("/home"); // Use navigate function to navigate to "/aboutus" path
-  // };
-
-
-   const handleAboutButtonClick = () => {
+  const handleAboutButtonClick = () => {
     navigate("/aboutus"); // Use navigate function to navigate to "/aboutus" path
   };
 
   const handleContactUsButtonClick = () => {
-    navigate("/contactus")
+    navigate("/contactus");
   };
 
   const handleGetInvolvedButtonClick = () => {
-    navigate("/getinvolved")
+    navigate("/getinvolved");
   };
 
   const handleNotificationsButtonClick = () => {
-    navigate("/notifications")
+    navigate("/notifications");
   };
 
   const handlePackagesButtonClick = () => {
-    navigate("/packages")
+    navigate("/packages");
   };
 
   const handleBasketButtonClick = () => {
-    navigate("/basket")
+    navigate("/basket");
   };
 
   const buttonVariants = {
     whileHover: { scale: 1.1 }, // Scale the button up on hover
-    whileTap: { scale: 0.9 }    // Scale the button down on tap
+    whileTap: { scale: 0.9 }, // Scale the button down on tap
   };
 
-  // const logoVariants = {
-  //   animate: {
-  //     scale: [1, 2, 2, 1, 1],
-  //     rotate: [0, 0, 270, 270, 0],
-  //     borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-  //   }
-  // };
-
   const images = [
-  "frontend/src/components/banner/bag_food.jpeg",
-  "frontend/src/components/banner/community-food.jpeg",
-  "frontend/src/components/banner/community.png",
-  "frontend/src/components/banner/food_bank1.jpeg",
-  "frontend/src/components/banner/food_bank2.webp",
-  "frontend/src/components/banner/food_bank3.jpeg",
-  "frontend/src/components/banner/food_bank4.webp",
-  ""
-];
-
+    "/images/bag_food.jpeg",
+    "/images/community-food.jpeg",
+    "/images/community.png",
+    "/images/food_bank1.jpeg",
+    "/images/food_bank2.webp",
+    "/images/food_bank3.jpeg",
+    "/images/food_bank4.webp",
+  ];
 
   return (
     <div className="app-container">
@@ -109,14 +90,12 @@ const App = () => {
         </div>
         <div className="header-right">
           {/* Rest of the buttons */}
-
-            <motion.button
+          <motion.button
             variants={buttonVariants}
             whileHover="whileHover"
             whileTap="whileTap"
             onClick={handleAboutButtonClick}
-  
-          > 
+          >
             About
           </motion.button>
           <motion.button
@@ -168,7 +147,7 @@ const App = () => {
           >
             Packages
           </motion.button>
-           <motion.button
+          <motion.button
             variants={buttonVariants}
             whileHover="whileHover"
             whileTap="whileTap"
@@ -192,13 +171,12 @@ const App = () => {
           /> */}
         </div>
 
-        <AppRoutes /> 
+        <AppRoutes />
         <Notifications />
-        {isSignUpOpen && <SignUpForm onClose={handleCloseSignUp} />} {/* Render SignUpForm only when isSignUpOpen is true */}
-        {isLogInOpen && <LogInForm onClose={handleCloseLogIn} />} 
-
+        {isSignUpOpen && <SignUpForm onClose={handleCloseSignUp} />}{" "}
+        {/* Render SignUpForm only when isSignUpOpen is true */}
+        {isLogInOpen && <LogInForm onClose={handleCloseLogIn} />}
         {/* Add your other content here */}
-       
       </main>
     </div>
   );
