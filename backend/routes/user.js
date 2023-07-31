23a2f0dedbd90f5da358623a1c23387d7c356c5a
Arbnor <arbnor-sfarca@hotmail.com>
2023-07-31 18:33:19 +0100

    Updated code to import jwt and define secret key

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,7 +3,10 @@ const User = require("../models/user");
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
+const jwt = require('jsonwebtoken'); // Import jwt module
 
+// Define your secret key here (e.g., replace 'your-secret-key' with your actual secret)
+const secret = 'your-secret-key';
 
 router.post('/register', userController.Create);
 
@@ -18,17 +21,14 @@ router.post("/login", async (req, res) => {
     return;
   }
 
-  // if (!user.comparePassword(password)) {
-  //   res.status(401).json({ message: "Invalid email or password" });
-  //   return;
-  // }
-
-  //1
+  if (!user.comparePassword(password)) {
+    res.status(401).json({ message: "Invalid email or password" });
+    return;
+  }
   console.log("Success")
 
-  // const token = jwt.sign({ userId: user._id }, secret, { expiresIn: 30 });
-  // res.json({ token });
-  //2
+  const token = jwt.sign({ userId: user._id }, secret, { expiresIn: 30 });
+  res.json({ token });
 });
 
 module.exports = router;
