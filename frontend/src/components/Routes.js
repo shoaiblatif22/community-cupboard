8f7db2ee2ba06d0a0b3edd7d63008876846027ab
Zara <zarabucknor@yahoo.co.uk>
2023-08-01 14:46:06 +0100

    Changed the path for home in routes

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,7 +1,7 @@
-// frontend/src/components/Routes.js
 
 import { Routes, Route } from "react-router-dom";
 import App from "./app/App.js";
+
 import SignUpForm from "./SignUp/SignUp";
 import LogInForm from "./login/LoginForm";
 import LogOutForm from "./login/logoutForm";
@@ -9,16 +9,18 @@ import AboutUs from "./aboutUs/AboutUs";
 import ContactUs from "./contactUs/ContactUs";
 import Basket from "./Basket/basket";
 import Packages from "./Packages/packages";
-import Checkout from "./Checkout/Checkout.js";
-import GetInvolved from "./getInvolved/getInvolved";
+import Checkout from "./Checkout/Checkout";
+
+import GetInvolved from "./getInvolved/getInvolved"
 import Notifications from "./notifications/notifications.js";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/home" element={<App />} />
+      <Route exact path="/" component={App} />
+      <Route path="/home" element={App} />
       <Route path="/getinvolved" element={<GetInvolved />} />
+
       <Route path="/signupform" element={<SignUpForm />} />
       <Route path="/login" element={<LogInForm />} />
       <Route path="/logout" element={<LogOutForm />} />
@@ -26,7 +28,7 @@ const AppRoutes = () => {
       <Route path="/contactus" element={<ContactUs />} />
       <Route path="/basket" element={<Basket />} />
       <Route path="/packages" element={<Packages />} />
-      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/Checkout" element={<Checkout />} />
       <Route path="/notifications" element={<Notifications />} />
     </Routes>
   );
